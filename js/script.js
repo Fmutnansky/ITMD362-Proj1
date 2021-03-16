10cc274c622792f5d8bd3b436bea0f6d691a6bfe
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2021-03-16 11:19:18 -0500

    Adding jQuery for submit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+const success = document.getElementById('success');
+
+form.onsubmit = submit;
+
+function submit(event) {
+  success.removeAttribute('hidden');
+
+  event.preventDefault();
+}
+
 //Automatic Slideshow
 var images = [
   ],

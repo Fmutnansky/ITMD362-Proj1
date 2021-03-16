8cccc0f536d7334ba3856d8673ca43f372063199
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2021-03-16 11:05:17 -0500

    Adding function to change image in JS slideshow

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ var images = [
   curIndex = 0;
   imgDuration = 4000; // Images Array
 
+//Change Image
+function changeImg(){
+  document.getElemtentById('slider').className += "fadeOut";
+    setTimeout(function() {
+      document.getElementById('slider').src = images[curIndex];
+      document.getElementById('slider').className = "";
+    },1000);
+  curIndex++;
+  if (curIndex == images.length) { curIndex = 0; }
+  setTimeout(changeImg, imgDuration);
+}
+
+window.onload = changeImg;

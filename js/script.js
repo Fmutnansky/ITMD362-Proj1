da6fd3731a1ad639b7c4c0151b2515d9d63b9280
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2021-04-13 21:26:29 -0500

    Moving slideshow

--- js/script.js
+++ js/script.js
@@ -15,26 +15,5 @@ function submit(event) {
 
   event.preventDefault();
 }
-};
-window.onload = function(){
-//Automatic Slideshow
-var images = [
-  "/media/Gaming.jpg",
-  "/media/LMG.jpg",
-  "/media/WaterBottle.jpg",
-  "/media/hw.jpg"],
-  curIndex = 0;
-  imgDuration = 4000; // Images Array
 
-// Change Image
-function changeImg() {
-  document.getElementById('slider').className += "fadeOut";
-    setTimeout(function() {
-      document.getElementById('slider').src = images[curIndex];
-      document.getElementById('slider').className = "";
-    },1000);
-  curIndex++;
-  if (curIndex == images.length) { curIndex = 0; }
-  setTimeout(changeImg, imgDuration);
-}
 };
